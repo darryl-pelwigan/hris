24f25ff8695c8d4bc137261b5116ae1a257ecb53
Darryl Pelwigan <darryl@wsatp.com>
2023-02-28 19:12:38 +0800

    Commit OLD Projects

---
+++ third_party/MPDF/ttfontdata/beholder.mtx.php
@@ -0,0 +1,38 @@
+<?php
+$name='Beholder-Regular';
+$type='TTF';
+$desc=array (
+  'CapHeight' => 700,
+  'XHeight' => 500,
+  'FontBBox' => '[-102 -133 627 846]',
+  'Flags' => 4,
+  'Ascent' => 846,
+  'Descent' => -133,
+  'Leading' => 0,
+  'ItalicAngle' => 0,
+  'StemV' => 87,
+  'MissingWidth' => 228,
+);
+$unitsPerEm=1000;
+$up=-75;
+$ut=50;
+$strp=250;
+$strs=50;
+$ttffile='/var/www/html/pcc/modules/application/third_party/MPDF/ttfonts/Beholder.ttf';
+$TTCfontID='0';
+$originalsize=27636;
+$sip=false;
+$smp=false;
+$BMPselected=false;
+$fontkey='beholder';
+$panose=' 0 0 0 0 0 0 0 0 0 0 0 0';
+$haskerninfo=false;
+$haskernGPOS=false;
+$hassmallcapsGSUB=false;
+$fontmetrics='win';
+// TypoAscender/TypoDescender/TypoLineGap = 750, -250, 0
+// usWinAscent/usWinDescent = 846, -133
+// hhea Ascent/Descent/LineGap = 750, -250, 9
+$useOTL=0x0000;
+$rtlPUAstr='';
+?>
